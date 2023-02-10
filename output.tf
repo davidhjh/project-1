fe6f4b26949b898660df84cf580524d796a0a679
David Huang <david.jh.huang@gmail.com>
2023-02-10 12:14:35 -0500

    Added an extra output to output.tf file.

--- output.tf
+++ output.tf
@@ -2,3 +2,8 @@ output "virtual_machine_id" {
   value       = resource.azurerm_virtual_machine.primary-vm.id
   description = "Virtual machine ID"
 }
+
+output "resource_group_location" {
+  value       = resource.azurerm_resource_group.project-resource-group.location
+  description = "Resource group location"
+}
